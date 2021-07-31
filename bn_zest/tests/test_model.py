57d629843c57bcb83c548d133d79f25bc6dd2887
Chris Joyner <c.joyner@qmul.ac.uk>
2021-07-31 11:52:07 +0100

    test model

---
+++ bn_zest/tests/test_model.py
@@ -0,0 +1,44 @@
+import unittest
+import base
+from bn_zest import BayesianNetwork, Node
+from models.construct_model import create_test_model
+
+class TestModel(base.ErrorTestMixin, unittest.TestCase):
+        
+    def setUp(self):
+
+        self.model = create_test_model()
+
+    def test_model_instance(self):
+        self.assertIsInstance(self.model, BayesianNetwork)
+
+    def test_model_variable_ids(self):
+        self.assertListEqual(self.model.variable_ids, ['a', 'b', 'c', 'd', 'e', 'f'])
+
+    def test_model_variable_names(self):
+        self.assertListEqual(self.model.variable_names, ['A', 'B', 'C', 'D', 'E', 'F'])
+
+    def test_get_variables(self):        
+        for idx in self.model.variable_ids:
+            self.assertIsInstance(self.model[idx], Node)
+
+    def test_model_prediction(self):
+        inputs = {
+            'a': 'No',
+            'c': 'Positive',
+            'd': 'Red'
+        }
+        
+        probs = self.model.predict_proba(X=inputs)
+        
+        outputs = {
+            'b': [0.6, 0.15, 0.25],
+            'e': [0.5, 0.5],
+            'f': [0.22260035069952577, 0.3346594875701657, 0.44274016173030845]
+        }
+
+        for idx, values in probs.items():            
+            self.assertListAlmostEqual(values, outputs[idx], places=6)
+
+if __name__ == '__main__':
+    unittest.main()
