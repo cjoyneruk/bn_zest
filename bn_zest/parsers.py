37b159b79ddec069d2897d87c92db19f6020eca4
Chris Joyner <c.joyner@qmul.ac.uk>
2021-07-31 14:41:35 +0100

    fix missing group key

--- bn_zest/parsers.py
+++ bn_zest/parsers.py
@@ -94,7 +94,7 @@ def from_cmpx(data, network=0, remove_disconnected_variables=True, force_summati
     var_list.loc[var_list['description'].isin(['', 'New Node']), 'description'] = None    
     var_list.loc[var_list['description'].isna(), 'description'] = None
     
-    var_list = var_list[[ 'id', 'name','description' , 'npt', 'states']]
+    var_list = var_list[[ 'id', 'name','description' , 'npt', 'states', 'group']]
 
     var_list['id'] = var_list['id'].apply(_sub_id)
 
